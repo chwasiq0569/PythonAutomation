528ea0e395dd09098661ef9a11f5d47ff7b2fa4b
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-03-25 16:05:45 +0500

    Created NewColumn added new values in it

--- index.py
+++ index.py
@@ -1,4 +1,13 @@
 import openpyxl as xl
 wb = xl.load_workbook("transactions.xlsx")
-print(wd)
-print("OPENPYXL")
+sheet = wb['Sheet1']
+# cell = sheet['a1']
+# cell = sheet.cell(1, 1)
+
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_Value = cell.value * 0.9
+    corrected_Value_cell = sheet.cell(row, 4)
+    corrected_Value_cell.value = corrected_Value
+    print(sheet.cell(row, 4).value)
+
